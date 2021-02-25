9959c3b8c8fad1d1d78ee08f813939ddaec265ca
simon schneemelcher <simonschneemelcher@gmail.com>
2021-02-25 12:59:05 +0100

    no need to cat into awk

--- cpu.sh
+++ cpu.sh
@@ -2,11 +2,11 @@
 # script that calculates cpu usage in percent
 # you need to execute cat /proc/stat /tmp/old beforehand
 
-old_idle=$(cat /tmp/old | awk 'NR>1 && NR < 6 {print $5+$6}')
-old_active=$(cat /tmp/old | awk 'NR>1 && NR < 6 {print $2+$3+$4+$7+$8+$9+$10}')
+old_idle=$(awk 'NR>1 && NR < 6 {print $5+$6}' /tmp/old)
+old_active=$(awk 'NR>1 && NR < 6 {print $2+$3+$4+$7+$8+$9+$10}' /tmp/old)
 
-new_idle=$(cat /proc/stat | awk 'NR>1 && NR < 6 {print $5+$6}')
-new_active=$(cat /proc/stat | awk 'NR>1 && NR < 6 {print $2+$3+$4+$7+$8+$9+$10}')
+new_idle=$(awk 'NR>1 && NR < 6 {print $5+$6}' /proc/stat)
+new_active=$(awk 'NR>1 && NR < 6 {print $2+$3+$4+$7+$8+$9+$10}' /proc/stat)
 
 old_total=$(paste <(echo "$old_idle") <(echo "$old_active") --delimiters ' ' | awk '{print $1+$2}')
 new_total=$(paste <(echo "$new_idle") <(echo "$new_active") --delimiters ' ' | awk '{print $1+$2}')
